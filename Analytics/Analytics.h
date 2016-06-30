4cc73369ed0348541e93a7c2c64a7208ddba2ef5
Tony Xiao <tonyx.ca@gmail.com>
2016-06-30 13:57:34 -0700

    Adding Framework target in addition to static lib target

--- Analytics/Analytics.h
+++ Analytics/Analytics.h
@@ -2,15 +2,21 @@
 //  Analytics.h
 //  Analytics
 //
-//  Created by Tony Xiao on 6/28/16.
+//  Created by Tony Xiao on 6/30/16.
 //  Copyright © 2016 Segment. All rights reserved.
 //
 
-#ifndef Analytics_h
-#define Analytics_h
+/*
+ The following two extern variables are disabled for now because Analytics.h can be used as static lib as well which would 
+ not have the FOUNDATION_EXPORT variables be properly defined
+//! Project version number for Analytics.
+FOUNDATION_EXPORT double AnalyticsVersionNumber;
+
+//! Project version string for Analytics.
+FOUNDATION_EXPORT const unsigned char AnalyticsVersionString[];
+*/
+// In this header, you should import all the public headers of your framework using statements like #import <Analytics/PublicHeader.h>
 
 #import "SEGAnalytics.h"
 #import "SEGAnalytics+Convenience.h"
 #import "SEGAnalyticsConfiguration.h"
-
-#endif /* Analytics_h */
