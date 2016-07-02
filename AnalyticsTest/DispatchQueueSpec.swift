dc205b5a6c3264acf574e17045fb553af9d6b644
Tony Xiao <tonyx.ca@gmail.com>
2016-07-01 23:15:47 -0700

    Add more tests for DispatchQueue

--- AnalyticsTest/DispatchQueueSpec.swift
+++ AnalyticsTest/DispatchQueueSpec.swift
@@ -15,6 +15,7 @@ class DispatchQueueSpec : QuickSpec {
     beforeEach { 
       queue = SEGDispatchQueue(label: "com.segment.test")
     }
+    
     it("reports isCurrentQueue correctly") {
       expect(queue.isCurrentQueue()) == false
       
@@ -31,7 +32,6 @@ class DispatchQueueSpec : QuickSpec {
       expect(isCurrentAsync) == false
       expect(isCurrentAsync).toEventually(beTrue())
     }
-    
     it("should never deadlock") {
       var done = false
       dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0)) {
@@ -51,6 +51,28 @@ class DispatchQueueSpec : QuickSpec {
         }
       }
       expect(done).toEventually(beTrue())
+    }
+    it("should run sync synchronously") {
+      var blockDidRun = false
+      queue.sync { blockDidRun = true }
+      expect(blockDidRun) == true
+    }
+    it("should run async asynchronously") {
+      expect(queue.isCurrentQueue()) == false
+      var didBlockRun = false
+      queue.async {
+        didBlockRun = true
+      }
+      expect(didBlockRun) == false
+      expect(didBlockRun).toEventually(beTrue())
+    }
+    it("should always run synchronously if already on queue") {
+      queue.sync {
+        expect(queue.isCurrentQueue()) == true
+        var didAsyncBlockRun = false
+        queue.async { didAsyncBlockRun = true }
+        expect(didAsyncBlockRun) == true
+      }
     }
   }
 }
